8fdcb181759233895088a4ed2d337b2bf9a5f5b2
CodeSchwert <2661899+CodeSchwert@users.noreply.github.com>
2020-01-20 11:59:48 +1300

    Cleanup script

--- convert.js
+++ convert.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const showdown = require('showdown');
 
+// Showdown options
+// https://github.com/showdownjs/showdown#valid-options
 const convertOpts = {
   ghCompatibleHeaderId: true,
   strikethrough: true,
@@ -16,6 +18,6 @@ const converter = new showdown.Converter(convertOpts);
 const policyDoc = fs.readFileSync('README.md', 'UTF-8');
 const html = converter.makeHtml(policyDoc);
 
-console.log(html);
-
 fs.writeFileSync('index.html', html);
+
+process.exit(0);
